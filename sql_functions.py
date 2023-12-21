22a160ea66f9adce1868614de57ca7a53dfedb21
Sascha <sascha@Saschas-Air.fritz.box>
2023-12-21 12:39:03 +0100

    update

--- sql_functions.py
+++ sql_functions.py
@@ -1,13 +1,12 @@
-# We import a method from the  modules to address environment variables and 
-# we use that method in a function that will return the variables we need from
-# .env to a dictionary we call sql_config
-
+import pandas as pd
+import sqlalchemy
 from dotenv import dotenv_values
+
 
 def get_sql_config():
     '''
-        loads credentials and parameters from .env file and returns a
-        dictionary containing those needed for sqlalchemy.create_engine()
+    loads credentials and parameters from '.env' file and returns a
+    dictionary containing those needed for sqlalchemy.create_engine()
     '''
     needed_keys = [
           'host'
@@ -18,58 +17,32 @@ def get_sql_config():
         , 'options'
     ]
     dotenv_dict = dotenv_values(".env")
-    sql_config = {
+    return {
         key: dotenv_dict[key] for key in needed_keys if key in dotenv_dict
     }
-    return sql_config
 
-# import sqlalchemy and pandas
-import sqlalchemy
-import pandas as pd
-
-
-# functions
-# =============================================================================
+def get_engine():
+    '''
+    returns sqlalchemy connection engine for the PostgreSQL database
+    (configured via '.env')
+    '''
+    return sqlalchemy.create_engine(
+          'postgresql://user:pass@host/database'
+        , connect_args=get_sql_config()
+    )
 
 def get_data(sql_query):
-   '''
-   connect to PostgreSQL database, run query and return result as a list
-   '''
-   # get the connection configuration dictionary using get_sql_config function
-   sql_config = get_sql_config()
-   # create a connection engine to the PostgreSQL server
-   engine = sqlalchemy.create_engine(
-        'postgresql://user:pass@host/database'
-      , connect_args=sql_config
-   )
-   # open conn session using 'with', execute the query, and return the results
-   with engine.begin() as conn: 
-      results = conn.execute(sql_query)
-      return results.fetchall()
+    '''
+    returns the result as a list after running sql_query on the PostgreSQL
+    database (configured via '.env')
+    '''
+    # open conn session using 'with', execute the query, and return the results
+    with get_engine().begin() as conn:
+        return conn.execute(sql_query).fetchall()
 
 def get_dataframe(sql_query):
-    ''' 
-    connect to PostgreSQL database, run query and return result as a dataframe
     '''
-    # get the connection configuration dictionary using get_sql_config function
-    sql_config = get_sql_config()
-    # create a connection engine to the PostgreSQL server
-    engine = sqlalchemy.create_engine(
-          'postgresql://user:pass@host/database'
-        , connect_args=sql_config
-    )
-    # return query result as DataFrame
-    return pd.read_sql_query(sql=sql_query, con=engine)
-
-# function to create sqlalchemy engine for writing data to a database
-def get_engine():
-    ''' 
-    connect to PostgreSQL database and return engine/connection,
-    e.g. for usage in toSQL()
+    returns the result as a dataframe after running sql_query on the PostgreSQL
+    database (configured via '.env')
     '''
-    sql_config = get_sql_config()
-    engine = sqlalchemy.create_engine(
-          'postgresql://user:pass@host/database'
-        , connect_args=sql_config
-    )
-    return engine
+    return pd.read_sql_query(sql=sql_query, con=get_engine())
