156841973ea4e09655dbf2be6aad2645bb811004
Sascha <sascha@Saschas-Air.fritz.box>
2024-01-08 16:27:09 +0100

    update

---
+++ _functions_data_files.py
@@ -0,0 +1,67 @@
+import os
+import requests
+import zipfile
+
+# -----------------------------------------------------------------------------
+
+data_dir = './data'
+
+# -----------------------------------------------------------------------------
+
+def get_file_path(file_name: str, check_exist: bool = False) -> str:
+    '''
+    returns full file path to 'file_name' (stored inside of 'data_dir'), also
+    checks existance depending on 'check_exist' and returns '' on fail
+    '''
+    file_path = os.path.join(data_dir, file_name)
+    if not check_exist or os.path.isfile(file_path):
+        return file_path
+    else:
+        return ''
+
+def download_file(url: str, file_name: str = '') -> str:
+    '''
+    downloads 'url' and stores it as 'file_name' (or one extracted from url)
+    into 'data_dir'; returns 'file_name' after successful save, '' otherwise
+    '''
+    # obtain 'file_name' from 'url', if not given
+    if file_name == '': file_name = os.path.basename(url)
+    # create full path to file
+    file_path = get_file_path(file_name)
+    # test and recieve file from 'url', store as 'file_name'
+    try:
+        response = requests.head(url)
+        if response.status_code != 200:
+            print('ERROR! url request failed with:', response.status_code)
+            return ''
+        else:
+            response = requests.get(url, allow_redirects=True, stream=True)
+            with open(file_path, mode='wb') as file:
+                for chunk in response.iter_content(chunk_size=1024 * 1024):
+                    file.write(chunk)
+            print('file downloaded:', file_name)
+            return file_name
+    except Exception as e:
+        print('ERROR! download failed:', e)
+        return ''
+
+def unzip_file(file_name: str) -> list[str]:
+    '''
+    unzips all contents of 'file_name' (stored inside of 'data_dir') into its
+    parent directory; returns list of extracted files or '[]' on fail
+    '''
+    # create full path to file, ensure it exists
+    file_path = get_file_path(file_name, check_exist=True)
+    if not file_path:
+        print('ERROR! file does not exist:', file_path)
+        return []
+    # extract all file contents to parent directory
+    try:
+        with zipfile.ZipFile(file_path, 'r') as zip_file:
+            zip_file_contents = zip_file.namelist()
+            zip_file.extractall(os.path.dirname(file_path))
+            print('files extracted:', zip_file_contents)
+            return zip_file_contents
+    except zipfile.BadZipFile:
+        print('ERROR! not a zip or a corrupted zip file')
+        return []
---
+++ _functions_sql.py
@@ -0,0 +1,60 @@
+import os
+import pandas
+import sqlalchemy
+
+from sqlalchemy.engine.base import Engine
+from dotenv import dotenv_values
+
+# -----------------------------------------------------------------------------
+
+env_file = './.env'
+
+# -----------------------------------------------------------------------------
+
+def get_sql_config() -> dict:
+    '''
+    loads credentials and parameters from 'env_file'; returns a dictionary
+    containing those needed further on, False otherwise
+    '''
+    # check if 'env_file' exists
+    if not os.path.isfile(env_file):
+        print('ERROR! file does not exist:', env_file)
+        return False
+    else:
+        needed_keys = [
+              'host'
+            , 'port'
+            , 'database'
+            , 'user'
+            , 'password'
+            , 'options'
+        ]
+        dotenv_dict = dotenv_values(env_file)
+        return {
+            key: dotenv_dict[key] for key in needed_keys if key in dotenv_dict
+        }
+
+def get_engine() -> Engine:
+    '''
+    returns sqlalchemy connection engine for the PostgreSQL database
+    (configured via 'env_file'); error handling is omitted
+    '''
+    return sqlalchemy.create_engine(
+          'postgresql://user:pass@host/database'
+        , connect_args=get_sql_config()
+    )
+
+def get_data(sql_query: str) -> list:
+    '''
+    returns the result as a list after running sql_query on the PostgreSQL
+    database (configured via 'env_file'); error handling is omitted
+    '''
+    with get_engine().begin() as connection:
+        return connection.execute(sql_query).fetchall()
+
+def get_dataframe(sql_query: str) -> pandas.DataFrame:
+    '''
+    returns the result as a pandas dataframe after running sql_query on the
+    PostgreSQL database (configured via 'env_file'); error handling is omitted
+    '''
+    return pandas.read_sql_query(sql=sql_query, con=get_engine())
